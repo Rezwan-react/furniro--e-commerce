3e9a997d303a45f876361f81c4b1e51eb141c2da
Rezwan-react <mdrazwanislam8@gmail.com>
2024-11-30 22:57:40 +0600

    added shopBanner, Filter, products

--- src/components/BreadCrumb/BreadCrumb.jsx
+++ src/components/BreadCrumb/BreadCrumb.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { MdArrowForwardIos } from "react-icons/md";
 
 function BreadCrumb() {
+
+  const location = useLocation()
+  const path = location.pathname.split("/")[1]
+
+
+
+
   return (
     <>
-        <ul className='flex justify-center items-center mt-2'>
+        <ul className='flex justify-center items-center gap-2 mt-2'>
             <li className='text-[16px] font-medium font-poppins text-[#000] '><Link to='/'>Home</Link></li>
-            <li><Link to='/Shop'>Shop</Link></li>
+            <li><MdArrowForwardIos /></li>
+            <li className='text-[16px] font-light font-poppins text-[#000] capitalize '><Link to='/Shop'>{path}</Link></li>
         </ul>
     </>
   )
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Shop.css'
 import BreadCrumb from '../BreadCrumb/BreadCrumb'
+import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
+import ProductsCard from '../Products/ProductsCard';
 
 function Shop() {
   return (
@@ -19,6 +21,43 @@ function Shop() {
           </div>
         </section>
       {/* ------------------------------- banner part end --------------------------- */}
+      {/* =============================== Filter part start ========================== */}
+        <section id='filter'>
+          <div className="container">
+            <div className="filter_row">
+              <div className="filter_col">
+                <div className="filter_text">
+                  <h3><HiOutlineAdjustmentsHorizontal /></h3>
+                  <h2> Filter </h2>
+                  <p>Showing 1–16 of 32 results</p>
+                </div>
+                <div className="filter_input">
+                  <label>Show</label>
+                  <input type="number" value='16' className='one' />
+                  <label>Short by</label>
+                  <input type="text" placeholder='Default' className='two'/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      {/* -------------------------------- Filter part end ---------------------------- */}
+      {/* ================================= products part start ====================== */}
+        <section id='products'>
+          < ProductsCard/>
+          < ProductsCard/>
+          < ProductsCard/>
+          < ProductsCard/>
+          < ProductsCard/>
+          < ProductsCard/>
+          < ProductsCard/>
+          < ProductsCard/>
+          < ProductsCard/>
+          < ProductsCard/>
+          < ProductsCard/>
+          < ProductsCard/>
+        </section>
+      {/* --------------------------------- products part end ------------------------  */}
     </>
   )
 }
